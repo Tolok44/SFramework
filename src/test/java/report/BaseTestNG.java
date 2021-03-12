e376ce98a598f67a930d4bbba12eec6f090404cf
Armando H <armando.hernandez.marin@gmail.com>
2021-03-11 23:12:33 -0600

    feat: added BaseTestNG class that automates report output for each test case, example usage was also added TestNGExample

---
+++ src/test/java/report/BaseTestNG.java
@@ -0,0 +1,99 @@
+package report;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class BaseTestNG {
+	
+		public String rptFilename = System.getProperty("user.dir") + "\\target\\ExtentReportResultsSelenium.html";
+		public WebDriver driver = null;
+						
+		private String getScreenShotsDir( ) {
+			File file = new File(rptFilename);
+			return file.getParent();
+		}
+		
+		@AfterMethod
+		public void afterMethod(ITestResult result) {
+			ExtentReports extent = createReport();
+			String testName = result.getName();
+			String headerMessage = "-";
+			String logMessage = "-";
+			LogStatus logStatus = LogStatus.PASS;
+			Throwable e = null;
+		   if (result.getStatus() == ITestResult.FAILURE) {
+			   saveScreenShot(testName);
+			   e = result.getThrowable();
+			   headerMessage =  "Test failed, click here for further details";
+			   logMessage = "Failure: " + e.toString();
+			   logStatus = LogStatus.FAIL;   
+		   }   
+		   
+		   ExtentTest test = extent.startTest(testName, headerMessage);
+		   test.log(logStatus, logMessage);
+		   flushReports(extent, test);
+		}
+	
+		private void saveScreenShot(String testName) {
+			if (driver != null) {
+				// take a screenshot
+				TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		        File scrFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		        File destFile = getDestinationFile(testName);
+		        try {
+		            FileUtils.copyFile(scrFile, destFile);
+		        } catch (IOException ioe) {
+		            throw new RuntimeException(ioe);
+		        }
+			}
+		}
+		
+		private ExtentReports createReport() {
+			ExtentReports extent = new ExtentReports(this.rptFilename, false);
+			extent.addSystemInfo("Host Name", "SoftwareTestingMaterial").addSystemInfo("Environment", "Automation Testing")
+					.addSystemInfo("User Name", "SDET Batch 1 - Reporting Team");
+			extent.loadConfig(new File(System.getProperty("user.dir") + "\\extent-config.xml"));
+			return extent;
+		}
+		
+		private File getDestinationFile(String testName) {
+	        String userDirectory = 	getScreenShotsDir();
+	        String date = getDateTime();
+	        String fileName = testName + "_" + date + ".png";
+	        //add date of today
+	        String dateForDir = date.substring(0, 10);
+	        String absoluteFileName = userDirectory + "/" + dateForDir + "/" + fileName;
+	 
+	        return new File(absoluteFileName);
+	    }
+		
+		 private String getDateTime() {
+		        Date date = Calendar.getInstance().getTime();
+		        // Display a date in day, month, year format
+		        DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy_HH_mm_ss");
+		        String today = formatter.format(date);
+		        return today;
+		    }
+
+		private void flushReports(ExtentReports extent, ExtentTest test) {
+			// ending test
+			extent.endTest(test);
+			// writing everything to document
+			extent.flush();
+		}
+}
---
+++ src/test/java/report/TestNGExample.java
@@ -0,0 +1,44 @@
+package report;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+// create your test extending BaseTestNG to leverage reporting capabilities
+public class TestNGExample extends BaseTestNG {
+
+	@BeforeSuite
+	public void beforeSuite() {
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		// Set the driver if this is a selenium test
+		driver = new ChromeDriver();
+		// you can customize the report name if omitted default will be used
+		rptFilename = System.getProperty("user.dir") + "\\target\\ExtentReportResultsSelenium.html";
+	}
+
+	// 2 example Tests
+	
+	@Test
+	public void myPassingExampleTest() {
+		driver.navigate().to("www.google.com");
+		int result = 1 + 1;
+		Assert.assertEquals(result, 2);
+	}
+
+	@Test
+	public void myFailExampleTest() {
+		// screenshot of failing tests will be saved by the base class
+		driver.navigate().to("www.google.com");
+		int result = 1 + 1;
+		Assert.assertEquals(result, 3);
+	}
+
+	// tear down driver
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("Tests finished");
+		driver.quit();
+	}
+}
