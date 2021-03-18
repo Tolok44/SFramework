b0f43872ce9c5c2bcc850a6cde352eb5d1da2438
Victor <padillamendoza@outlook.com>
2021-03-18 16:51:28 -0600

    Updated SMethods with new selenium actions

--- src/main/java/runner/SMethods.java
+++ src/main/java/runner/SMethods.java
@@ -3,7 +3,9 @@ import org.openqa.selenium.WebElement;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.Properties;
 
 
 public class SMethods {
@@ -19,13 +21,56 @@ public class SMethods {
         driver.navigate().to(url);
     }
 
-    public void click(String selector, String value){
-        this.typeOfAction(selector, value).click();
-    }
+
 
     public void write(String selector, String LocatorValue, String text){
         this.typeOfAction(selector, LocatorValue).sendKeys(text);
     }
+
+    public void clear(String selector, String locatorValue){
+        this.typeOfAction(selector, locatorValue).clear();
+    }
+
+    public void refresh(){
+        driver.navigate().refresh();
+    }
+
+    public boolean elementExists(String type, String lcoatorValue){
+        WebElement a;
+
+        switch(type){
+            case "id":
+                if (driver.findElement(By.id(lcoatorValue))!=null){
+                    return true;
+                }
+                break;
+            case "name":
+                if(driver.findElement(By.name(lcoatorValue))!=null){
+                  return true;
+                }
+                break;
+            case "tagName":
+                if(driver.findElement(By.tagName(lcoatorValue))!=null){
+                    return true;
+                }
+                break;
+            case "cssSelector":
+                if(driver.findElement(By.cssSelector(lcoatorValue))!=null){
+                    return true;
+                }
+                break;
+            case "xpath":
+                if(driver.findElement(By.xpath(lcoatorValue))!=null){
+                    return true;
+                }
+                break;
+            default:
+                return false;
+        }
+            return false;
+    }
+
+
 
     public WebElement typeOfAction(String type, String value){
         WebElement a;
@@ -50,6 +95,90 @@ public class SMethods {
                 throw new IllegalStateException("Unexpected value: " + type);
         }
         return a;
+    }
+
+
+
+
+
+
+
+    public String realizar(Properties p, String keyword, String LocatorType, String LocatorValue, String value, String description) throws Exception {
+        System.out.println("realizando operacion....");
+        switch(keyword.toLowerCase()){
+            case "goto":
+                System.out.println(description);
+                driver.get(p.getProperty(value));
+                return "";
+            case "click":
+                System.out.println(description);
+                driver.findElement(getLocator(p, LocatorType, LocatorValue)).click();
+                return "";
+            case "type":
+                System.out.println(description);
+                driver.findElement(getLocator(p, LocatorType, LocatorValue)).sendKeys(value);
+                return "";
+            case "gettext":
+                System.out.println(description);
+                return driver.findElement(getLocator(p, LocatorType, LocatorValue)).getText();
+            case "clear":
+                System.out.println(description);
+                driver.findElement(getLocator(p, LocatorType, LocatorValue)).clear();
+                return "";
+            case "refresh":
+                System.out.println(description);
+                driver.navigate().refresh();
+                return "";
+            case "select":
+                System.out.println(description);
+                selectAction(p, LocatorType, LocatorValue, value);
+                return "";
+            case "comparetext":
+                System.out.println(description);
+                if (compareText(driver.findElement(getLocator(p, LocatorType, LocatorValue)), value)){
+                    return "";
+                }else{
+                    throw new Exception("El texto a comparar no es igual");
+                }
+            default:
+                throw new Exception("Keyword erronea");
+        }
+    }
+
+    private By getLocator(Properties p, String LocatorType, String LocatorValue) throws Exception {
+        switch(LocatorType.toLowerCase()) {
+            case "id":
+                return By.id(p.getProperty(LocatorValue));
+            case "xpath":
+                return By.xpath(p.getProperty(LocatorValue));
+            case "css":
+                return By.cssSelector(p.getProperty(LocatorValue));
+            case "name":
+                return By.name(p.getProperty(LocatorValue));
+            case "class":
+                return By.className(p.getProperty(LocatorValue));
+            case "text":
+                return By.linkText(p.getProperty(LocatorValue));
+            case "partial text":
+                return By.linkText(p.getProperty(LocatorValue));
+            default:
+                throw new Exception("Wrong object type");
+        }
+    }
+
+
+    public void selectAction(Properties p, String LocatorType, String LocatorValue, String value) throws Exception {
+        Select item = new Select(driver.findElement(getLocator(p, LocatorType,LocatorValue)));
+        item.selectByVisibleText(value);
+    }
+
+    public boolean compareText(WebElement e, String value) {
+        if (e.getText().equals(value)){
+            return true;
+        }else{
+            return false;
+        }
+
     }
 }
 
