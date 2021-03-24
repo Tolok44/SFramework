960c57ae3aa088078233aa14e1014386d2c26152
Armando H <armando.hernandez.marin@gmail.com>
2021-03-24 14:19:16 -0600

    added details to reports, added screenshot and robot screenshot ehn alert is present

--- src/test/java/report/RunnerSelenium.java
+++ src/test/java/report/RunnerSelenium.java
@@ -2,9 +2,18 @@ package report;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.Markup;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 import excelManager.ReadExcelFile;
+
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.OutputType;
+
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -16,86 +25,168 @@ import runner.GUI;
 import runner.SMethods;
 import testCase.StepSelenium;
 
+import java.awt.AWTException;
+import java.awt.Desktop;
+import java.awt.HeadlessException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import javax.imageio.ImageIO;
 
 public class RunnerSelenium {
 
+	WebDriver driver;
+	ExtentHtmlReporter htmlReporter;
+	ExtentReports extent;
+	ExtentTest test;
+	String path;
+	String fileName;
+	String tcSelected;
+	String reportFilename;
 
-    WebDriver driver;
-    ExtentHtmlReporter htmlReporter;
-    ExtentReports extent;
-    ExtentTest test;
-    String path;
-    String fileName;
-    String tcSelected;
+	@BeforeClass
+	public void setClass() throws IOException {
+		reportFilename = String.format("target/report_%s.html", getDateTime());
+		htmlReporter = new ExtentHtmlReporter(reportFilename);
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
 
-    @BeforeClass
-    public void setClass() throws IOException {
-        htmlReporter = new ExtentHtmlReporter("extent.html");
-        extent = new ExtentReports();
-        extent.attachReporter(htmlReporter);
+		System.setProperty("webdriver.chrome.driver", ".\\Driver\\chromedriver.exe");
 
-        System.setProperty("webdriver.chrome.driver", ".\\Driver\\chromedriver.exe");
+	}
 
+	@DataProvider(name = "pasos")
+	Object[][] getData() throws IOException {
+		GUI gui = new GUI();
+		List<List<StepSelenium>> testCases = new ArrayList<>();
+		String[] info = gui.showGui();
+		path = info[0];
+		fileName = info[1];
+		tcSelected = info[2];
+		String[] cases = tcSelected.split("-");
+		for (int i = 0; i < cases.length; i++) {
+			List<StepSelenium> steps = GetTCData.getStepSelenium(path, fileName, cases[i]);
+			testCases.add(steps);
+		}
 
-    }
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		Object[][] datos = new Object[testCases.size()][1];
+		for (int row = 0; row < testCases.size(); row++) {
+			datos[row][0] = testCases.get(row);
+		}
+		return datos;
+	}
 
-    @DataProvider(name="pasos")
-    Object[][] getData() throws IOException{
-        GUI gui = new GUI();
-        List<List<StepSelenium>> testCases = new ArrayList<>();
-        String[] info = gui.showGui();
-        path = info[0];
-        fileName = info[1];
-        tcSelected = info[2];
-        String[] cases = tcSelected.split("-");
-        for(int i=0; i<cases.length;i++) {
-            List<StepSelenium> steps = GetTCData.getStepSelenium(path, fileName, cases[i]);
-            testCases.add(steps);
-        }
+	// test de data provider
+	@Test(dataProvider = "pasos")
+	public void testCase(List<StepSelenium> pasos) throws IOException {
+		ReadObject object = new ReadObject();
+		Properties allObjects = object.getProperties();
+		SMethods fw = new SMethods(driver);
+		StepSelenium paso1 = pasos.get(0);
+		test = extent.createTest(paso1.getName(), paso1.getDescription());
+		Integer stepNo = 1;
+		for (StepSelenium step : pasos) {
+			try {
+				// you can customize the report name if omitted default will be used
+				fw.realizar(allObjects, step.getKeyword(), step.getLocatorType(), step.getLocatorValue(),
+						step.getValue(), step.getStepDescription());
+//				test.pass(step.getStepDescription());
+				test.pass(getReportStepDescription(stepNo, step));
+				if (step.getTakeScreenShot().equals("y")) {
+					addDetails(test, step);
+				}
+			} catch (Exception | AssertionError e) {
+				test.fail(getReportStepDescription(stepNo, step));
+				addDetails(test, step);
+				test.error(e.getMessage());
+			}
+			stepNo++;
+		}
 
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        Object[][] datos = new Object[testCases.size()][1];
-        for(int row=0;row<testCases.size();row++) {
-            datos[row][0]=testCases.get(row);
-        }
-        return datos;
-    }
+	}
 
+	@AfterSuite
+	public void tearDown() {
+		extent.flush();
+		driver.quit();
+		try {
+			Desktop.getDesktop().open(new File(reportFilename));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    //test de data provider
-    @Test(dataProvider="pasos")
-    public void testCase(List<StepSelenium> pasos) throws IOException{
-        ReadObject object = new ReadObject();
-        Properties allObjects = object.getProperties();
-        SMethods fw = new SMethods(driver);
-        StepSelenium paso1 = pasos.get(0);
-        test = extent.createTest(paso1.getName(), paso1.getDescription());
-        for(StepSelenium step : pasos) {
-            try {
-                // you can customize the report name if omitted default will be used
-                fw.realizar(allObjects, step.getKeyword(), step.getLocatorType(), step.getLocatorValue(), step.getValue(), step.getStepDescription());
-                test.pass(step.getStepDescription());
-            }
-            catch(Exception | AssertionError e) {
-                test.fail(step.getStepDescription());
-                test.error(e.getMessage());
-            }
-        }
+	private String getReportStepDescription(Integer stepNo, StepSelenium step) {
+		return String.format("step #%s: %s", stepNo.toString(), step.getStepDescription());
+	}
 
-    }
+	public String captureScreen() {
+		TakesScreenshot newScreen = (TakesScreenshot) driver;
+		return newScreen.getScreenshotAs(OutputType.BASE64);
+	}
 
+	private String getDateTime() {
+		Date date = Calendar.getInstance().getTime();
+		DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy_HH_mm_ss");
+		String today = formatter.format(date);
+		return today;
+	}
 
-    @AfterSuite
-    public void tearDown() {
-        extent.flush();
-        driver.quit();
-    }
+	public boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException Ex) {
+			return false;
+		}
+	}
+
+	private void addDetails(ExtentTest test, StepSelenium step) throws IOException {
+		String[][] tableData = { { "keyword", step.getKeyword() }, { "locator type", step.getLocatorType() },
+				{ "locator value", step.getLocatorValue() }
+				// ,{ "value", step.getValue() }
+		};
+		Markup table = MarkupHelper.createTable(tableData);
+		test.log(Status.INFO, table);
+
+		if (!isAlertPresent()) {
+			test.log(Status.INFO, "after step screenshot",
+					MediaEntityBuilder.createScreenCaptureFromBase64String(captureScreen()).build());
+		} else {
+			try {
+				String b64ss = getRobotScreenCapture();
+				test.log(Status.INFO, "after step ROBOT screenshot",
+						MediaEntityBuilder.createScreenCaptureFromBase64String(b64ss).build());
+			} catch (HeadlessException | AWTException | IOException ex) {
+				System.out.println(ex.toString());
+			}
+		}
+	}
+
+	private String getRobotScreenCapture() throws HeadlessException, AWTException, IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		BufferedImage image = new Robot()
+				.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+		ImageIO.write(image, "png", out);
+		byte[] bytes = out.toByteArray();
+		return Base64.getEncoder().encodeToString(bytes);
+
+	}
 }
